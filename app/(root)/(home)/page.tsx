0e3a98a13b1f789af50bfdce73278d934fb3f233
guifeel <guifeel@qq.com>
2023-12-12 13:58:22 +0800

    Create style for localsearchbar filters wrapper

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse gap-4 sm:flex-row sm:items-center sm:justify-between">
+        <h1 className="h1-bold text-dark100_light900">所有问题</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900">
+            提问问题
+          </Button>
+        </Link>
+      </div>
+      <div className="text-dark100_light900 mt-11 flex  justify-between max-sm:flex-col sm:items-center">
+        <div>LocalSearcbar</div>
+        <div>Filters</div>
+      </div>
+    </>
+  );
 }
